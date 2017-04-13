f692531c087d31b470f5c1dc3184e2ea05cc2752
bincap <bincap@magmaship.com>
2017-04-13 02:49:42 +0100

    Genpage: generates the exit pages

--- README.md
+++ README.md
@@ -5,9 +5,18 @@ This is the HTML Page for https://bip148.org
 
 ## Help with design and content: Submit a pull request!
 
+## This pages uses crude hack in bash to generate some parts from templates
+
+To regenerate htmls (needed to get all HTML files as needed for page):
+review the code of this bash script (just few lines), and run it:
+```
+cd pagegen/ && bash generate.sh
+```
 
 ## This Website Uses LESS (CSS)
 
 To build: `lessc styles/all.less css/all.css`
+
 (If you don't have LESS installed already: `npm install -g less`)
+
 
---
+++ pagegen/generate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function template_execute_level2() {
+	cat goto_level2.html | sed -e "s@VARIABLE_LINK@${2}@g" > "../goto_${1}.html"
+}
+
+function template_execute_level3() {
+	cat goto_level3.html | sed -e "s@VARIABLE_LINK@${2}@g" > "../goto_${1}.html"
+}
+
+template_execute_level3 'github_bip148' 'https://github.com/bitcoin/bips/blob/master/bip-0148.mediawiki' 
+template_execute_level2 'github_coinruntool' 'https://github.com/magmaship/coinruntool'
+template_execute_level2 'github_bip148org' 'https://github.com/magmaship/bip148org'
+
+template_execute_level3 'uasfco' 'http://www.uasf.co/'
+template_execute_level3 'freenodeirc' 'https://webchat.freenode.net/?channels=#bip148,##uasf'
+
