5a636b6e038176a27761ecfef2e8e2ce99c56b07
bincap <bincap@magmaship.com>
2017-07-16 20:30:45 +0100

    Green download; Twitter links

--- pagegen/generate.sh
+++ pagegen/generate.sh
@@ -11,6 +11,8 @@ function template_execute_level3() {
 template_execute_level3 'github_bip148' 'https://github.com/bitcoin/bips/blob/master/bip-0148.mediawiki' 
 template_execute_level2 'github_coinruntool' 'https://github.com/magmaship/coinruntool'
 template_execute_level2 'github_bip148org' 'https://github.com/magmaship/bip148org'
+template_execute_level2 'twitter_bip148' 'https://twitter.com/bip148'
+template_execute_level2 'twitter_cmp2bldx' 'https://twitter.com/cmp2bldx'
 
 template_execute_level3 'uasfco' 'http://www.uasf.co/'
 template_execute_level3 'freenodeirc' 'https://webchat.freenode.net/?channels=#bip148,##uasf'
