55460506a11df42bfbff6be23c3ad5b39e0e7622
bincap <bincap@magmaship.com>
2017-07-20 14:07:18 +0100

    Footer/donation fixes

--- pagegen/generate.sh
+++ pagegen/generate.sh
@@ -14,6 +14,10 @@ template_execute_level2 'github_bip148org' 'https://github.com/magmaship/bip148o
 template_execute_level2 'twitter_bip148' 'https://twitter.com/bip148'
 template_execute_level2 'twitter_cmp2bldx' 'https://twitter.com/cmp2bldx'
 
+template_execute_level2 'bitcoinuasf_org' 'https://bitcoinuasf.org/'
+template_execute_level3 'our_addr_btc' 'https://blockchain.info/address/182q5NpvWT5d6BttkNoiGzMje7wSqQUUvk'
+template_execute_level3 'our_addr_btc_bip148' 'https://uasf-explorer.satoshiportal.com/address/182q5NpvWT5d6BttkNoiGzMje7wSqQUUvk'
+
 template_execute_level3 'uasfco' 'http://www.uasf.co/'
 template_execute_level3 'uasfco_signs_current_windows' 'https://github.com/UASF/gitian.sigs/tree/master/0.14.2-uasfsegwit1.0-win-unsigned'
 template_execute_level3 'uasfco_signs_current_linux' 'https://github.com/UASF/gitian.sigs/tree/master/0.14.2-uasfsegwit1.0-linux'
